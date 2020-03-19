e30a2464cb8678199d74ad8e3e476ff9d5e12b9b
Isma <didouhismael@gmail.com>
2020-03-19 22:17:28 +0100

    finished box model

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ const reload = require("reload");
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("src"));
 
 // routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/src/index.html"));
+});
+app.get("/box-model", (req, res) => {
+  res.sendFile(path.join(__dirname + "/src/box-model/index.html"));
 });
 
 // Reload code here
